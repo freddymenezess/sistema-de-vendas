8b723f655800c8204f3b05229d5c3d915bbd201e
Freddy Menezes <friedrichprogrammer@gmail.com>
2026-01-21 21:37:42 +0100

    Refatorar rotas
    Criei rotas a partir de um Layout principal para facilitar a manutenção futura do código.

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,33 +1,28 @@
 import { Routes, Route } from 'react-router-dom';
 import PrivateRoute from '@auth/PrivateRoute';
 
+import MainLayout from '@templates/MainLayout/MainLayout';
 import Home from '@pages/Home/Home';
 import Login from '@pages/Login/Login';
 import Dashboard from '@pages/Dashboard/Dashboard';
 
+
 function AppRoutes() {
   return (
     <Routes>
-      <Route path='*' element={<Login />} />
-      <Route
-        path="/login"
-        element={<Login />}
-      />
+      <Route path="*" element={<Login />} />
+      <Route path="/login" element={<Login />} />
 
       <Route
-        path="/"
         element={
           <PrivateRoute>
-            <Home />
-          </PrivateRoute>}
-      />
-      <Route
-        path="/dashboard"
-        element={
-          <PrivateRoute>
-            <Dashboard />
-          </PrivateRoute>}
-      />
+            <MainLayout />
+          </PrivateRoute>
+        }
+      >
+        <Route path="/home" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Route>
     </Routes>
   );
 }
