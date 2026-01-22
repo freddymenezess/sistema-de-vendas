925ac9c67cff50e1a6cd588d0b3ac5cc98047ea3
Freddy Menezes <friedrichprogrammer@gmail.com>
2026-01-22 22:44:40 +0100

    Aplicar nova regra de negócio a Avatar

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,13 +1,13 @@
 import styles from './Avatar.module.css';
 
-function Avatar({ name, role }) {
+function Avatar({ name, role, className }) {
   const initials = name
     .split(' ')
     .map((part) => part[0].toUpperCase())
     .join('')
     .slice(0, 2);
 
-  return <div className={`${styles.avatar} ${styles[role]}`}>{initials}</div>;
+  return <div className={`${styles.avatar} ${styles[role]} ${className}`}>{initials}</div>;
 }
 
 export default Avatar;
