48a17d26d8d2deea31d1b532fcd871ba3211da7a
Freddy Menezes <friedrichprogrammer@gmail.com>
2026-01-22 22:45:19 +0100

    Aplicar resolução de bug a Statics

--- src/templates/MainLayout/MainLayout.jsx
+++ src/templates/MainLayout/MainLayout.jsx
@@ -1,5 +1,4 @@
 import { Outlet } from 'react-router-dom';
-import Header from '@components/Header/Header';
 import NavBar from '@components/NavBar/NavBar';
 import styles from './MainLayout.module.css';
 
