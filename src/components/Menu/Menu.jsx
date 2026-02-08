3979623a93a29e6d60be39930309219843eaa1a9
Freddy Menezes <friedrichprogrammer@gmail.com>
2026-02-08 14:58:09 +0100

    Corrigir bud Cards e Aplicar correção ao menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -14,28 +14,30 @@ function Menu({ className }) {
   }
 
   return (
-    <div className={`${styles.menu} ${className}`}>
-      <h3>Prdutos no carrinho</h3>
-      <img
-        src="/prods/bronzeador.png"
-        alt="Imagem de produto para compra"
-        className={styles.img}
-      />
-      <p className={styles.price}>
-        <strong>Preço:</strong> 12000KZS
-      </p>
-      <div className={styles.qtdArea}>
-        <label htmlFor="qtd" className={styles.lblQtd}>
-          Quantidade
-          <div className={`${styles.less} ${styles.btn}`} onClick={handleDec}>
-            -
-          </div>
-          <input type="number" name="qtd" id="qtd" value={qtd} />
-          <div className={`${styles.more} ${styles.btn}`} onClick={handleInc}>
-            +
-          </div>
-        </label>
-        <p className={styles.finalPrice}>Preço final a pagar: {finalPrice}</p>
+    <div className={`${styles.menu} ${className} flex`}>
+      <div className={styles.main}>
+        <h3>Prdutos no carrinho</h3>
+        <img
+          src="/prods/bronzeador.png"
+          alt="Imagem de produto para compra"
+          className={styles.img}
+        />
+        <p className={styles.price}>
+          <strong>Preço:</strong> 12000KZS
+        </p>
+        <div className={styles.qtdArea}>
+          <label htmlFor="qtd" className={styles.lblQtd}>
+            Quantidade
+            <div className={`${styles.less} ${styles.btn}`} onClick={handleDec}>
+              -
+            </div>
+            <input type="number" name="qtd" id="qtd" value={qtd} />
+            <div className={`${styles.more} ${styles.btn}`} onClick={handleInc}>
+              +
+            </div>
+          </label>
+          <p className={styles.finalPrice}>Preço final a pagar: {finalPrice}</p>
+        </div>
       </div>
       <button type="button" className={styles.closeBuy}>
         Finalizar compra
